df6b1836c8d7d98f7d84b5c317d3471af99375cb
lastrucci01 <u20430168@tuks.co.za>
2023-05-12 13:10:27 +0200

    added some file.handler functions

--- src/sentence_alignment/file_handler.py
+++ src/sentence_alignment/file_handler.py
@@ -30,3 +30,42 @@ def write_tokens_to_txt(tokens, edition, lang):
     new = open(path, 'w')
     for token in tokens:
         new.write('{}\n'.format(token))
+
+def read_file_as_array(edition_path, txt_path): # -> str
+    """
+    ### Reads file as an array.
+
+    Generally used to read tokenised data for further processing
+    """
+    file_path = Path(token_data_path / edition_path / txt_path)
+    return open(file_path, 'r').readlines() 
+
+def append_to_final_csv(src_lang, src_sentences, tgt_lang, tgt_sentences , sim_scores):
+    """
+    ### Appends to the ML aligned lang pairs .csv - creates it if it doesn't exist
+    #### Params
+        -   src_lang: source lang (str)
+        -   src_sentences: source sentence tokens (list)
+        -   src_vector: source sentence vectors (list)
+        -   tgt_lang: target lang (str)
+        -   tgt_sentences: target sentence tokens (list)
+        -   tgt_vector: target sentence vectors (list)
+        -   sim_scores: confidence scores between the pairing (list)
+    """
+    data = {
+        'src_text' : src_sentences,
+        'tgt_text' : tgt_sentences,
+        'cosine_score' : sim_scores
+    } # build a dictonary of the info
+
+    df = pandas.DataFrame(data) #turn into a pandas DF
+
+    csv_path = Path('aligned_{}_{}.csv'.format(src_lang, tgt_lang)) # Path of lang aligned csv
+    if not os.path.exists(output_data_path): # if data/sentence_alignment doesnt exist
+        os.makedirs(output_data_path) # create it 
+
+    if os.path.exists(Path(output_data_path / csv_path)): # if .csv does exist
+        df.to_csv(Path(output_data_path / csv_path), mode='a',header=False, index=False) # append with no headers
+    else: 
+        df.to_csv(Path(output_data_path / csv_path), mode='w',header=True, index=False) # create with headers
+
